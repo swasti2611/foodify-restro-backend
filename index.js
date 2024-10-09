0bc37388b030434f7991a4a5440bbf2033540a34
Swati jagtap <swatijagtap5060@gmail.com>
2024-10-09 17:25:45 +0530

    Zomato-backend-

--- index.js
+++ index.js
@@ -20,20 +20,18 @@ mongoose.connect("mongodb+srv://swati2611:1488rCJ3VcxGXXpT@cluster0.giiopqv.mong
 })
 
 
-
-
 app.use(cors({
-  origin: 'https://zomato-clone-psi-six.vercel.app',  // Replace with your frontend domain
-  methods: 'GET,POST',  // Specify allowed methods (optional)
-  credentials: true }  
-));
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+  origin:"https://zomato-clone-psi-six.vercel.app",
+  methods:"GET,POST,PUT,DELETE",
+  credentials:true
+}));
+app.use(express.json());
+
 // setup session
 app.use(session({
-    secret:"1234secreatkey",
-    resave:false,
-    saveUninitialized:true
+  secret:process.env.SESSION_SECRET,
+  resave:false,
+  saveUninitialized:true
 }))
 
 // setuppassport
@@ -41,59 +39,139 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 passport.use(
-    new OAuth2Strategy({
-        clientID:process.env.CLIENT_ID,
-        clientSecret:process.env.CLIENT_SECREATE,
-        callbackURL:"/auth/google/callback",
-        scope:["profile","email"]
-    },
-    async(accessToken,refreshToken,profile,done)=>{
-        try {
-            let user = await userdb.findOne({googleId:profile.id});
+  new OAuth2Strategy({
+      clientID:clientid,
+      clientSecret:clientsecret,
+      callbackURL:"/auth/google/callback",
+      scope:["profile","email"]
+  },
+  async(accessToken,refreshToken,profile,done)=>{
+      try {
+          let user = await userdb.findOne({googleId:profile.id});
 
-            if(!user){
-                user = new userdb({
-                    googleId:profile.id,
-                    displayName:profile.displayName,
-                    email:profile.emails[0].value,
-                    image:profile.photos[0].value
-                });
+          if(!user){
+              user = new userdb({
+                  googleId:profile.id,
+                  displayName:profile.displayName,
+                  email:profile.emails[0].value,
+                  image:profile.photos[0].value
+              });
 
-                await user.save();
-            }
+              await user.save();
+          }
 
-            return done(null,user)
-        } catch (error) {
-            return done(error,null)
-        }
-    }
-    )
+          return done(null,user)
+      } catch (error) {
+          return done(error,null)
+      }
+  }
+  )
 )
 
 passport.serializeUser((user,done)=>{
-    done(null,user);
+  done(null,user);
 })
 
 passport.deserializeUser((user,done)=>{
-    done(null,user);
+  done(null,user);
 });
 
 // initial google ouath login
 app.get("/auth/google",passport.authenticate("google",{scope:["profile","email"]}));
 
 app.get("/auth/google/callback",passport.authenticate("google",{
-    successRedirect:"https://zomato-clone-psi-six.vercel.app",
+  successRedirect:"https://zomato-clone-psi-six.vercel.app",
     failureRedirect:"https://zomato-clone-psi-six.vercel.app/login"
 }))
+
 app.get("/login/sucess",async(req,res)=>{
+
+  if(req.user){
+      res.status(200).json({message:"user Login",user:req.user})
+  }else{
+      res.status(400).json({message:"Not Authorized"})
+  }
+})
+
+app.get("/logout",(req,res,next)=>{
+  req.logout(function(err){
+      if(err){return next(err)}
+      res.redirect("https://zomato-clone-psi-six.vercel.app");
+  })
+})
+
+// app.use(cors({
+//   origin: 'https://zomato-clone-psi-six.vercel.app',  // Replace with your frontend domain
+//   methods: 'GET,POST',  // Specify allowed methods (optional)
+//   credentials: true }  
+// ));
+// app.use(express.json())
+// app.use(express.urlencoded({extended:true}))
+// setup session
+// app.use(session({
+//     secret:"1234secreatkey",
+//     resave:false,
+//     saveUninitialized:true
+// }))
+
+// // setuppassport
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// passport.use(
+//     new OAuth2Strategy({
+//         clientID:process.env.CLIENT_ID,
+//         clientSecret:process.env.CLIENT_SECREATE,
+//         callbackURL:"/auth/google/callback",
+//         scope:["profile","email"]
+//     },
+//     async(accessToken,refreshToken,profile,done)=>{
+//         try {
+//             let user = await userdb.findOne({googleId:profile.id});
+
+//             if(!user){
+//                 user = new userdb({
+//                     googleId:profile.id,
+//                     displayName:profile.displayName,
+//                     email:profile.emails[0].value,
+//                     image:profile.photos[0].value
+//                 });
+
+//                 await user.save();
+//             }
+
+//             return done(null,user)
+//         } catch (error) {
+//             return done(error,null)
+//         }
+//     }
+//     )
+// )
+
+// passport.serializeUser((user,done)=>{
+//     done(null,user);
+// })
+
+// passport.deserializeUser((user,done)=>{
+//     done(null,user);
+// });
+
+// // initial google ouath login
+// app.get("/auth/google",passport.authenticate("google",{scope:["profile","email"]}));
+
+// app.get("/auth/google/callback",passport.authenticate("google",{
+//     successRedirect:"https://zomato-clone-psi-six.vercel.app",
+//     failureRedirect:"https://zomato-clone-psi-six.vercel.app/login"
+// }))
+// app.get("/login/sucess",async(req,res)=>{
     
-const transporter = nodemailer.createTransport({
-    service: 'gmail', // or use any other email service
-    auth: {
-      user: process.env.EMAIL_USER, // your email address
-      pass: process.env.EMAIL_PASS, // your email password or app-specific password
-    },
-  });
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail', // or use any other email service
+//     auth: {
+//       user: process.env.EMAIL_USER, // your email address
+//       pass: process.env.EMAIL_PASS, // your email password or app-specific password
+//     },
+//   });
   
   const sendEmail = (to, subject, text) => {
     const mailOptions = {
@@ -128,15 +206,15 @@ const transporter = nodemailer.createTransport({
   }
 
     
-})
+// })
 
 
-app.get("/logout",(req,res,next)=>{
-    req.logout(function(err){
-        if(err){return next(err)}
-        res.redirect("https://zomato-clone-psi-six.vercel.app");
-    })
-})
+// app.get("/logout",(req,res,next)=>{
+//     req.logout(function(err){
+//         if(err){return next(err)}
+//         res.redirect("https://zomato-clone-psi-six.vercel.app");
+//     })
+// })
 app.get("/",(req,res)=>{
   res.send("hello")
 })
